fa0502d77a7bee0301fdda5b350b97ace47f45ef
Guz117 <cristian.guzman117@gmail.com>
2022-01-31 15:49:35 +0100

    snack 4

---
+++ snack 4/index.php
@@ -0,0 +1,19 @@
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+$randomNumber = [];
+
+
+while (count($randomNumber) < 15) {
+    $random = rand(1, 30);
+    if (!in_array($random, $randomNumber)) {
+        $randomNumber[] = $random;
+    }
+}
+
+echo "<pre>";
+var_dump($randomNumber);
+"</pre>";
+
+?>
